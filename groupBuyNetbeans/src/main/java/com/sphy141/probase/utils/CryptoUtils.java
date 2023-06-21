5af821ae5e2b298635c5d475ae63bf930fef6769
dimsot <129656621+mitsosgla@users.noreply.github.com>
2023-06-21 08:35:56 +0200

    crypt

--- groupBuyNetbeans/src/main/java/com/sphy141/probase/utils/CryptoUtils.java
+++ groupBuyNetbeans/src/main/java/com/sphy141/probase/utils/CryptoUtils.java
@@ -12,6 +12,9 @@ package com.sphy141.probase.utils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoUtils {
     
@@ -38,5 +41,65 @@ public class CryptoUtils {
             e.printStackTrace();
         }
         return null;
+    }//hashString
+
+/*επειδή ο hash-256 είναι μονόδρομος αλγόριθμος κρυπρογάφισης 
+χρησιμοποιώ τον αλγόριθμο AES προκειμένου να δημιουργήσω μία κρυπτογράφιση για τα δεδομένα της βάσης που είνια ευαίσθητα πχ χρήστη και επιχειρήσεων*/
+    
+       public static String encrypt(String plainText, String key) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }//hashAESString
+
+    public static String decrypt(String encryptedText, String key) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }//hashAESString
+    
+      public static String encryptDouble(double number, String key) {
+        try {
+            String plainText = Double.toString(number);
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-}
+
+    public static double decryptDouble(String encryptedText, String key) {
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+            String decryptedText = new String(decryptedBytes);
+            return Double.parseDouble(decryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0.0;
+    }
+    
+    
+    
+}//class
